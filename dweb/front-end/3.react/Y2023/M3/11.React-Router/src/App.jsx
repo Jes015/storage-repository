a22bf06f1ab87b0a35baf816d8413ecbe75393df
Jes015 <>
2023-03-06 23:39:58 +0000

    practice frontend 9

--- dweb/front-end/3.react/Y2023/M3/11.React-Router/src/App.jsx
+++ dweb/front-end/3.react/Y2023/M3/11.React-Router/src/App.jsx
@@ -1,22 +1,18 @@
+// MUI
 import { CssVarsProvider } from '@mui/joy/styles'
 import CssBaselinse from '@mui/joy/CssBaseline'
+
+// React
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Header from './components/Header'
+
+// Routes
+import Router from './Router'
 
 export default function App () {
   return (
     <CssVarsProvider defaultMode='dark' modeStorageKey='mode-toggle-demo'>
       <CssBaselinse />
-      <div>
-        <Header />
-        <Router>
-          <Routes>
-            <Route path='/bro' element={<h1>Hola bro</h1>} />
-            <Route path='/' element={<h1>Hola home</h1>} />
-          </Routes>
-        </Router>
-      </div>
+      <Router />
     </CssVarsProvider>
   )
 }
